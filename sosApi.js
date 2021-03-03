04b955bb0a59dba5e80b2a99129945848e73c689
ArnoBunckens <arno.bunckens@student.ucll.be>
2021-03-03 11:29:55 +0100

    Show vid

--- sosApi.js
+++ sosApi.js
@@ -290,8 +290,8 @@ app.all("/users/setFeedback",  bodyParser.text({type: '*/*'}), async (req, res) 
     console.log( "BODY:" + JSON.stringify(req.body))
     var body = JSON.parse(req.body)
 
-    var vname = body.video.toString();
-    var feedback = body.feedback.toString();
+    var vname = body.video;
+    var feedback = body.feedback
 
     console.log(vname)
     console.log(feedback)
